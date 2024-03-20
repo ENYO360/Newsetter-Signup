14e2e33843dbc23a1ef689affb650f105ea90026
Shaibu Moses <106714361+ENYO360@users.noreply.github.com>
2024-03-20 17:48:12 +0100

    Delete NewsLetter-signUp directory

--- NewsLetter-signUp/script2.js
+++
@@ -1,8 +0,0 @@
-const submitEmail = document.getElementById('submitEmail')
-const dismissBtn = document.getElementById('dismissbtn')
-
-submitEmail.textContent = localStorage.getItem("savedEmail") + "."
-
-dismissBtn.addEventListener('click', function() {
-    window.location = 'index.html'
-})
